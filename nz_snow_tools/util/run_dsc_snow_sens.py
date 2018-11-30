70d455bcaaad88e144626fc113d711f36469bab9
Jono Conway <jono@bodekerscientific.com>
2018-11-30 14:24:09 +1300

    updated for precip sensitivity runs

--- nz_snow_tools/util/run_dsc_snow_sens.py
+++ nz_snow_tools/util/run_dsc_snow_sens.py
@@ -17,27 +17,27 @@ years = range(2000, 2016 + 1)
 
 
 inp_id = 'norton'  #['jobst_ucc', 'norton', 'vcsn']
-for tempchange in range(-5, 2, 1): # temperature change from -5 to + 1 K
+for tempchange in [-2, 0, 1]: # temperature change from -5 to + 1 K
+    for precipchange in [20, 50, 100]:
+        config_id_out = '5_t{}_p{}'.format(tempchange,precipchange)  # string identifying suffix to the namelist configuration file used by the model
+        for year in years:
+            # read in namelist
+            nml = f90nml.read(path_to_namelist)
+            # modify for given run
+            nml['ddf_config']['TopographyFile'] = projects + '/DSC-Snow/runs/input_DEM/Clutha_nztm250m_topo_no_ice_origin{}.nc'.format(origin)
+            nml['ddf_config']['ClimateSource'] = temp + '/DSC-Snow/input_data_hourly/met_inp_Clutha_nztm250m_{}_{}_{}.nc'.format(year, inp_id, origin)
+            nml['ddf_config']['OutputFile'] = temp + '/DSC-Snow/runs/output/clutha_nztm250m_erebus/Clutha_nztm250m_{}_{}_{}_{}.nc'.format(year, inp_id,
+                                                                                                                                          config_id_out, origin)
+            nml['ddf_config']['starttime'][0] = year
+            nml['ddf_config']['endtime'][0] = year + 1
+            nml['ddf_config']['tempchange'] = tempchange
 
-    config_id_out = '5_{}'.format(tempchange)  # string identifying suffix to the namelist configuration file used by the model
-    for year in years:
-        # read in namelist
-        nml = f90nml.read(path_to_namelist)
-        # modify for given run
-        nml['ddf_config']['TopographyFile'] = projects + '/DSC-Snow/runs/input_DEM/Clutha_nztm250m_topo_no_ice_origin{}.nc'.format(origin)
-        nml['ddf_config']['ClimateSource'] = temp + '/DSC-Snow/input_data_hourly/met_inp_Clutha_nztm250m_{}_{}_{}.nc'.format(year, inp_id, origin)
-        nml['ddf_config']['OutputFile'] = temp + '/DSC-Snow/runs/output/clutha_nztm250m_erebus/Clutha_nztm250m_{}_{}_{}_{}.nc'.format(year, inp_id,
-                                                                                                                                      config_id_out, origin)
-        nml['ddf_config']['starttime'][0] = year
-        nml['ddf_config']['endtime'][0] = year + 1
-        nml['ddf_config']['tempchange'] = tempchange
+            # write namelist
+            path_to_output_namelist = projects + '/DSC-Snow/runs/input/clutha_nztm250m_erebus/ddf_config_{}_{}_{}_{}.txt'.format(year, inp_id, config_id_out,
+                                                                                                                                 origin)
+            nml.write(path_to_output_namelist, force=True)
 
-        # write namelist
-        path_to_output_namelist = projects + '/DSC-Snow/runs/input/clutha_nztm250m_erebus/ddf_config_{}_{}_{}_{}.txt'.format(year, inp_id, config_id_out,
-                                                                                                                             origin)
-        nml.write(path_to_output_namelist, force=True)
-
-        # run model
-        args = [path_to_ddf_run, path_to_output_namelist]
-        subprocess.call(args)
+            # run model
+            args = [path_to_ddf_run, path_to_output_namelist]
+            subprocess.call(args)
 
