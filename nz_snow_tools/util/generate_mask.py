fd3c94a9a386a450f5d670167ee587e491fb54b7
jonoconway <33852994+jonoconway@users.noreply.github.com>
2020-05-06 09:21:58 +1200

    naming of mask

--- nz_snow_tools/util/generate_mask.py
+++ nz_snow_tools/util/generate_mask.py
@@ -8,44 +8,40 @@ import numpy as np
 from nz_snow_tools.util.utils import create_mask_from_shpfile, setup_nztm_dem
 # from nz_snow_tools.met.interp_met_data_hourly_vcsn_data import
 import os
+
 #
 # # os.environ['PROJ_LIB'] = r'C:\miniconda\envs\nz_snow27\Library\share'
 
-#catchments = ['Wilkin']
+# catchments = ['Wilkin']
 # catchments = ['Clutha','Wilkin','Wanaka northern inflows','Upper Dart','Rees', 'Shotover', 'Teviot','Taieri','Upper Matukituki','Roaring Meg','Pomahaka','Motutapu',\
 #               'Moonlight Creek','Matukituki', 'Manuherikia','Luggate Creek', 'Lochy','Lindis',\
 #               'Kawarau','Greenstone','Hawea','Fraser','Clutha above Clyde Dam','Cardrona','Arrow' ,'Bannockburn Creek', 'Nevis'] # string identifying the catchment to run. must match the naming of the catchment shapefile
 
-output_dem = 'nztm250m'  # identifier for output dem
-dem_folder = ''  # folder with dem (option)
-dem = 'modis_nz_dem_250m'
+dem = 'modis_nz_dem_250m' # identifier for modis grid - extent specified below
+mask_folder = '/nesi/nobackup/niwa00026/Observation/Snow_RemoteSensing/catchment_masks'  # location of numpy catchment mask. must be writeable if mask_created == False
+catchment_shp_folder = '/nesi/nobackup/niwa00026/Observation/Snow_RemoteSensing/Catchments'  # shapefile containing polyline or polygon of catchment in WGS84
 
-mask_folder = '/nesi/nobackup/niwa00026/Observation/Snow_RemoteSensing/catchment_masks'   # location of numpy catchment mask. must be writeable if mask_created == False
-
-catchment_shp_folder = '/nesi/nobackup/niwa00026/Observation/Snow_RemoteSensing/Catchments' # shapefile containing polyline or polygon of catchment in WGS84
-
-
+# read names of shapefiles
 contents = os.listdir(catchment_shp_folder)
-shps = [s for s in contents if ".shp" in s and ".xml" not in s]
+shps = [s.split('.')[0] for s in contents if ".shp" in s and ".xml" not in s]
 
 # calculate model grid etc:
 # output DEM
 
 if dem == 'clutha_dem_250m':
-    dem_file = dem_folder + dem + '.tif'
-    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(dem_file)
+    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(None, extent_w=1.2e6, extent_e=1.4e6, extent_n=5.13e6, extent_s=4.82e6,
+                                                                          resolution=250, origin='bottomleft')
 
 if dem == 'si_dem_250m':
-    dem_file = dem_folder + dem + '.tif'
-    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(dem_file, extent_w=1.08e6, extent_e=1.72e6, extent_n=5.52e6, extent_s=4.82e6,
-                                                                          resolution=250)
+    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(None, extent_w=1.08e6, extent_e=1.72e6, extent_n=5.52e6, extent_s=4.82e6,
+                                                                          resolution=250, origin='bottomleft')
 if dem == 'modis_si_dem_250m':
     nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(None, extent_w=1.085e6, extent_e=1.72e6, extent_n=5.52e6, extent_s=4.82e6,
-                                                                          resolution=250)
+                                                                          resolution=250, origin='bottomleft')
 
 if dem == 'modis_nz_dem_250m':
     nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(None, extent_w=1.085e6, extent_e=2.10e6, extent_n=6.20e6, extent_s=4.70e6,
-                                                                          resolution=250)
+                                                                          resolution=250, origin='bottomleft')
 
 for catchment in shps:
     if '.shp' in catchment:
