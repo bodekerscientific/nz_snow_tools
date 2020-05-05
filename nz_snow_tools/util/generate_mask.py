2e89b0c8e1e71663c8ea7f96cf9e2c4cade0a6a2
jonoconway <33852994+jonoconway@users.noreply.github.com>
2020-05-05 16:06:04 +1200

    updating to - find shapefiles in folder and generate mask for each - NZ modis extent

--- nz_snow_tools/util/generate_mask.py
+++ nz_snow_tools/util/generate_mask.py
@@ -7,21 +7,25 @@ import numpy as np
 from nz_snow_tools.util.utils import create_mask_from_shpfile, setup_nztm_dem
 # from nz_snow_tools.met.interp_met_data_hourly_vcsn_data import
 import os
-os.environ['PROJ_LIB']=r'C:\miniconda\envs\nz_snow27\Library\share'
+#
+# # os.environ['PROJ_LIB'] = r'C:\miniconda\envs\nz_snow27\Library\share'
 
-catchments = ['Wilkin']
+#catchments = ['Wilkin']
 # catchments = ['Clutha','Wilkin','Wanaka northern inflows','Upper Dart','Rees', 'Shotover', 'Teviot','Taieri','Upper Matukituki','Roaring Meg','Pomahaka','Motutapu',\
 #               'Moonlight Creek','Matukituki', 'Manuherikia','Luggate Creek', 'Lochy','Lindis',\
 #               'Kawarau','Greenstone','Hawea','Fraser','Clutha above Clyde Dam','Cardrona','Arrow' ,'Bannockburn Creek', 'Nevis'] # string identifying the catchment to run. must match the naming of the catchment shapefile
 
 output_dem = 'nztm250m'  # identifier for output dem
-dem_folder = '' #'Z:/GIS_DATA/Topography/DEM_NZSOS/'
+dem_folder = ''  # 'Z:/GIS_DATA/Topography/DEM_NZSOS/'
 dem = 'modis_si_dem_250m'
-mask_dem = True  # boolean to set whether or not to mask the output dem
-mask_created = False  # boolean to set whether or not the mask has already been created
+
 mask_folder = r'C:\Users\conwayjp\OneDrive - NIWA\projects\DSC Snow\MODIS\masks'  # location of numpy catchment mask. must be writeable if mask_created == False
 # shapefile containing polyline or polygon of catchment in WGS84
 catchment_shp_folder = r'C:\Users\conwayjp\OneDrive - NIWA\projects\DSC Snow\MODIS\catchments'
+
+
+contents = os.listdir(catchment_shp_folder)
+shps = [s for s in contents if ".shp" in s and ".xml" not in s]
 
 # calculate model grid etc:
 # output DEM
@@ -32,13 +36,23 @@ if dem == 'clutha_dem_250m':
 
 if dem == 'si_dem_250m':
     dem_file = dem_folder + dem + '.tif'
-    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(dem_file, extent_w=1.08e6, extent_e=1.72e6, extent_n=5.52e6,
-                                                                          extent_s=4.82e6,
+    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(dem_file, extent_w=1.08e6, extent_e=1.72e6, extent_n=5.52e6, extent_s=4.82e6,
                                                                           resolution=250)
 if dem == 'modis_si_dem_250m':
     nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(None, extent_w=1.085e6, extent_e=1.72e6, extent_n=5.52e6, extent_s=4.82e6,
                                                                           resolution=250)
-for catchment in catchments:
-    mask_shpfile = catchment_shp_folder + '/{}.shp'.format(catchment)
-    mask = create_mask_from_shpfile(lat_array, lon_array, mask_shpfile)
+
+if dem == 'modis_nz_dem_250m':
+    nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(None, extent_w=1.085e6, extent_e=2.10e6, extent_n=6.20e6, extent_s=4.70e6,
+                                                                          resolution=250)
+
+for catchment in shps:
+    if '.shp' in catchment:
+        mask_shpfile = catchment_shp_folder + '/{}'.format(catchment)
+    else:
+        mask_shpfile = catchment_shp_folder + '/{}.shp'.format(catchment)
+    if dem == 'modis_nz_dem_250m':
+        mask = create_mask_from_shpfile(y_centres, x_centres, mask_shpfile)
+    else:
+        mask = create_mask_from_shpfile(lat_array, lon_array, mask_shpfile)
     np.save(mask_folder + '/{}_{}.npy'.format(catchment, dem), mask)
