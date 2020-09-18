875f9a38d32bf933c87f2357086850bcac5c11ea
jonoconway <33852994+jonoconway@users.noreply.github.com>
2020-09-18 13:39:52 +1200

    change to hydrological year (starts 1st April) don't process sw input data for clark model paths to files on NESI hpcf

--- nz_snow_tools/snow/run_snow_otf.py
+++ nz_snow_tools/snow/run_snow_otf.py
@@ -8,61 +8,73 @@ import pickle
 import numpy as np
 import datetime as dt
 import matplotlib.pylab as plt
+import json
+import os
 
 from nz_snow_tools.snow.clark2009_snow_model import calc_dswe
 from nz_snow_tools.util.utils import create_mask_from_shpfile, make_regular_timeseries, convert_datetime_julian_day
 from nz_snow_tools.met.interp_met_data_hourly_vcsn_data import load_new_vscn, interpolate_met, process_precip, daily_to_hourly_swin_grids, \
     daily_to_hourly_temp_grids, setup_nztm_dem, setup_nztm_grid_netcdf, trim_lat_lon_bounds
 
+
+run_id = 'cl09_default'
+met_inp = 'vcsn_norton' #identifier for input meteorology
 # model options
 which_model = 'dsc_snow'  # string identifying the model to be run. options include 'clark2009', 'dsc_snow' # future will include 'fsm'
 
 # time and grid extent options
-years_to_take = range(2000, 2016 + 1)  # [2013 + 1]  # range(2001, 2013 + 1)
-catchment = 'Clutha'  # string identifying the catchment to run. must match the naming of the catchment shapefile
-output_dem = 'nztm250m'  # identifier for output dem
-dem_folder = 'Z:/GIS_DATA/Topography/DEM_NZSOS/'
-dem = 'si_dem_250m'
-mask_dem = True  # boolean to set whether or not to mask the output dem
+years_to_take = range(2000, 2019 + 1)  # [2013 + 1]  # range(2001, 2013 + 1)
+catchment = 'SI'  # string identifying the catchment to run. must match the naming of the catchment shapefile
+output_dem = 'si_dem_250m'
+mask_dem = False  # boolean to set whether or not to mask the output dem
 mask_created = True  # boolean to set whether or not the mask has already been created
-mask_folder = 'T:/DSC-Snow/Masks'  # location of numpy catchment mask. must be writeable if mask_created == False
-mask_shpfile = 'Z:/GIS_DATA/Hydrology/Catchments/{}.shp'.format(
-    catchment)  # shapefile containing polyline or polygon of catchment in WGS84. Not needed if mask_created==True
-catchment_shp_folder = 'Z:/GIS_DATA/Hydrology/Catchments'
+mask_folder = None  # location of numpy catchment mask. must be writeable if mask_created == False
+# mask_shpfile = 'Z:/GIS_DATA/Hydrology/Catchments/{}.shp'.format(
+#     catchment)  # shapefile containing polyline or polygon of catchment in WGS84. Not needed if mask_created==True
+# catchment_shp_folder = 'Z:/GIS_DATA/Hydrology/Catchments'
 
 # output options
-output_folder = 'T:/DSC-Snow/nz_snow_runs/baseline_clutha2'
+
+dem_folder = '/nesi/project/niwa00004/jonoconway'  # dem used for output #'C:/Users/conwayjp/OneDrive - NIWA/Data/GIS_DATA/Topography/DEM_NZSOS'#
+output_folder = '/nesi/nobackup/niwa00004/jonoconway/snow_sims_nz/vcsn'  # snow model output
+data_folder = '/nesi/nobackup/niwa00004/jonoconway'  # input meteorology #'C:/Users/conwayjp/OneDrive - NIWA/projects/CARH2101/tn_file_compilation'#
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 
 # paths to input met data
-nc_file_rain = nc.Dataset('T:/newVCSN/rain_vclim_clidb_1972010100_2017102000_south-island_p05_daily.nc', 'r')
+nc_file_rain = nc.Dataset('/nesi/nobackup/niwa00026/VCSN/Rain/rain_vclim_clidb_1972010100_2020090200_south-island_p05_daily_netcdf4.nc', 'r')
 #nc_file_tmax = nc.Dataset('T:/newVCSN/tmax_vclim_clidb_1972010100_2017102000_south-island_p05_daily.nc', 'r')
-nc_file_tmax = nc.Dataset('T:/newVCSN/tmax_N2_1980010100_2017073100_south-island_p05_daily.nc', 'r')
+nc_file_tmax = nc.Dataset('/nesi/nobackup/niwa00026/VCSN/TMax_Norton/tmax_vclim_clidb_Norton_1972010100_2020090200_south-island_p05_daily_netcdf4.nc', 'r')
 #nc_file_tmin = nc.Dataset('T:/newVCSN/tmin_vclim_clidb_1972010100_2017102000_south-island_p05_daily.nc', 'r')
-nc_file_tmin = nc.Dataset('T:/newVCSN/tmin_N2_1980010100_2017073100_south-island_p05_daily.nc', 'r')
-nc_file_srad = nc.Dataset('T:/newVCSN/srad_vclim_clidb_1972010100_2017102000_south-island_p05_daily.nc', 'r')
+nc_file_tmin = nc.Dataset('/nesi/nobackup/niwa00026/VCSN/TMin_Norton/tmin_vclim_clidb_Norton_1972010100_2020090200_south-island_p05_daily_netcdf4.nc', 'r')
+nc_file_srad = nc.Dataset('/nesi/nobackup/niwa00026/VCSN/SRad/srad_vclim_clidb_1972010100_2020090200_south-island_p05_daily_netcdf4.nc', 'r')
 
 
 # configuration dictionary containing model parameters.
 config = {}
 config['tacc'] = 274.16
-config['tmelt'] = 274.16
+config['tmelt'] = 273.16
 # clark2009 melt parameters
 config['mf_mean'] = 5.0
 config['mf_amp'] = 5.0
 config['mf_alb'] = 2.5
 config['mf_alb_decay'] = 5.0
-config['mf_ros'] = 0
-config['mf_doy_max_ddf'] = 35
+config['mf_ros'] = 2.5
+config['mf_doy_max_ddf'] = 356
 # dsc_snow melt parameters
-config['tf'] = 0.2 * 24  # hamish 0.13
-config['rf'] = 0.005 * 24  # hamish 0.0075
+config['tf'] = 0.05 * 24  # hamish 0.13
+config['rf'] = 0.0108 * 24  # hamish 0.0075
 # albedo parameters
 config['dc'] = 11.0
 config['tc'] = 10.0
 config['a_ice'] = 0.42
 config['a_freshsnow'] = 0.90
 config['a_firn'] = 0.62
-config['alb_swe_thres'] = 20
+config['alb_swe_thres'] = 10
+config['ros'] = False
+config['ta_m_tt'] = False
 
 # load met grid (assume same for all input data)
 vcsn_elev = np.flipud(nc_file_rain.variables['elevation'][:])
@@ -82,21 +94,22 @@ vcsn_dt4 = nc.num2date(nc_file_srad.variables['time'][:], nc_file_srad.variables
 
 # calculate model grid etc:
 # output DEM
-dem_file = dem_folder + dem + '.tif'
-if dem == 'clutha_dem_250m':
+dem_file = dem_folder + output_dem + '.tif'
+if output_dem == 'clutha_dem_250m':
     nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(dem_file)
 
-if dem == 'si_dem_250m':
+if output_dem == 'si_dem_250m':
     nztm_dem, x_centres, y_centres, lat_array, lon_array = setup_nztm_dem(dem_file, extent_w=1.08e6, extent_e=1.72e6, extent_n=5.52e6, extent_s=4.82e6,
                                                                           resolution=250)
 
 if mask_dem == True:
     # Get the masks for the individual regions of interest
     if mask_created == True:  # load precalculated mask
-        mask = np.load(mask_folder + '/{}_{}.npy'.format(catchment, dem))
+        mask = np.load(mask_folder + '/{}_{}.npy'.format(catchment, output_dem))
     else:  # create mask and save to npy file
-        mask = create_mask_from_shpfile(lat_array, lon_array, mask_shpfile)
-        np.save(mask_folder + '/{}_{}.npy'.format(catchment, dem), mask)
+        print('need to precalculate mask file')
+    #     mask = create_mask_from_shpfile(lat_array, lon_array, mask_shpfile)
+    #     np.save(mask_folder + '/{}_{}.npy'.format(catchment, output_dem), mask)
     # Trim down the number of latitudes requested so it all stays in memory
     lats, lons, elev, northings, eastings = trim_lat_lon_bounds(mask, lat_array, lon_array, nztm_dem, y_centres, x_centres)
     _, _, trimmed_mask, _, _ = trim_lat_lon_bounds(mask, lat_array, lon_array, mask.copy(), y_centres, x_centres)
@@ -112,10 +125,10 @@ else:
 for year_to_take in years_to_take:
     # specify the days to run (output is at the end of each day)
     # out_dt = np.asarray(make_regular_timeseries(dt.datetime(year_to_take, 7, 1), dt.datetime(year_to_take, 7, 2), 86400))
-    out_dt = np.asarray(make_regular_timeseries(dt.datetime(year_to_take, 1, 1), dt.datetime(year_to_take + 1, 1, 1), 86400))
+    out_dt = np.asarray(make_regular_timeseries(dt.datetime(year_to_take-1, 4, 1), dt.datetime(year_to_take, 4, 1), 86400))
 
     # set up output netCDF:
-    out_nc_file = setup_nztm_grid_netcdf(output_folder + '/snow_out_{}_{}_{}.nc'.format(catchment, output_dem, year_to_take),
+    out_nc_file = setup_nztm_grid_netcdf(output_folder + '/snow_out_{}_{}_{}_{}_{}.nc'.format(met_inp, catchment, output_dem, run_id, year_to_take),
                                          None, ['swe', 'acc', 'melt'],
                                          out_dt, northings, eastings, lats, lons, elev)
 
@@ -140,24 +153,18 @@ for year_to_take in years_to_take:
     for ii, dt_t in enumerate(out_dt[:-1]):
         # load one day of precip and shortwave rad data
         precip_daily = load_new_vscn('rain', dt_t, nc_file_rain, nc_opt=True, single_dt=True, nc_datetimes=vcsn_dt)
-        sw_rad_daily = load_new_vscn('srad', dt_t, nc_file_srad, nc_opt=True, single_dt=True, nc_datetimes=vcsn_dt4)
         # load 3 days of temperature data (1 either side of day of interest - needed for interpolation of temperature)
         dts_to_take = make_regular_timeseries(dt_t - dt.timedelta(days=1), dt_t + dt.timedelta(days=1), 86400)
         max_temp_daily = load_new_vscn('tmax', dts_to_take, nc_file_tmax, nc_opt=True, nc_datetimes=vcsn_dt2)
         min_temp_daily = load_new_vscn('tmin', dts_to_take, nc_file_tmin, nc_opt=True, nc_datetimes=vcsn_dt3)
-        # precip_daily_data = precip_daily.data
-        # precip_daily_data[(precip_daily_data == precip_daily.fill_value)] = np.nan
-        # sw_rad_daily_data = sw_rad_daily.data
-        # sw_rad_daily_data[(sw_rad_daily_data==sw_rad_daily.fill_value)]=np.nan
+
         # interpolate data to fine grid
         hi_res_precip = interpolate_met(precip_daily.filled(np.nan), 'rain', vcsn_lons, vcsn_lats, vcsn_elev_interp, lons, lats, elev, single_dt=True)
-        hi_res_sw_rad = interpolate_met(sw_rad_daily.filled(np.nan), 'srad', vcsn_lons, vcsn_lats, vcsn_elev_interp, lons, lats, elev, single_dt=True)
         hi_res_max_temp = interpolate_met(max_temp_daily.filled(np.nan), 'tmax', vcsn_lons, vcsn_lats, vcsn_elev_interp, lons, lats, elev)
         hi_res_min_temp = interpolate_met(min_temp_daily.filled(np.nan), 'tmin', vcsn_lons, vcsn_lats, vcsn_elev_interp, lons, lats, elev)
         # mask out areas we don't want/need
         if mask is not None:
             hi_res_precip[trimmed_mask == 0] = np.nan
-            hi_res_sw_rad[trimmed_mask == 0] = np.nan
             hi_res_max_temp[:, trimmed_mask == 0] = np.nan
             hi_res_min_temp[:, trimmed_mask == 0] = np.nan
         # interpolate to hourly step
@@ -165,7 +172,6 @@ for year_to_take in years_to_take:
                                                          one_day=True)  # TODO: align to 9am-9am - currently pretends is midnight-midnight
 
         hourly_dt = np.asarray(make_regular_timeseries(dt_t, dt_t + dt.timedelta(hours=23), 3600))
-        hourly_swin = daily_to_hourly_swin_grids(hi_res_sw_rad, lats, lons, hourly_dt, single_dt=True)
         # air temperature is three part sinusoidal between min at 8am and max at 2pm. NOTE original VCSN data has correct timestamp - ie. minimum to 9am, maximum from 9am.
         # use three days but only keep middle day
         hourly_temp = daily_to_hourly_temp_grids(hi_res_max_temp, hi_res_min_temp)
@@ -175,13 +181,22 @@ for year_to_take in years_to_take:
         day_weightings.extend(day_weightings_1)
         hourly_doy = convert_datetime_julian_day(hourly_dt)
 
+        if which_model == 'dsc_snow':
+            sw_rad_daily = load_new_vscn('srad', dt_t, nc_file_srad, nc_opt=True, single_dt=True, nc_datetimes=vcsn_dt4)
+            hi_res_sw_rad = interpolate_met(sw_rad_daily.filled(np.nan), 'srad', vcsn_lons, vcsn_lats, vcsn_elev_interp, lons, lats, elev, single_dt=True)
+            if mask is not None:
+                hi_res_sw_rad[trimmed_mask == 0] = np.nan
+            hourly_swin = daily_to_hourly_swin_grids(hi_res_sw_rad, lats, lons, hourly_dt, single_dt=True)
+
         # calculate snow and output to netcdf
         for i in range(len(hourly_dt)):
             # d_snow += dtstep / 86400.0
-
-            swe, d_snow, melt, acc = calc_dswe(swe, d_snow, hourly_temp[i], hourly_precip[i], hourly_doy[i], 3600, sw=hourly_swin[i], which_melt=which_model,
+            if which_model == 'dsc_snow':
+                swe, d_snow, melt, acc = calc_dswe(swe, d_snow, hourly_temp[i], hourly_precip[i], hourly_doy[i], 3600, sw=hourly_swin[i], which_melt=which_model,
+                                                   **config)
+            else:
+                swe, d_snow, melt, acc = calc_dswe(swe, d_snow, hourly_temp[i], hourly_precip[i], hourly_doy[i], 3600, which_melt=which_model,
                                                **config)
-
             # print swe[0]
             bucket_melt = bucket_melt + melt
             bucket_acc = bucket_acc + acc
@@ -202,5 +217,6 @@ for year_to_take in years_to_take:
 
     out_nc_file.close()
 
-    pickle.dump(config, open(output_folder + '/config_{}_{}_{}.pkl'.format(catchment, output_dem, year_to_take), 'wb'), -1)
-    pickle.dump(day_weightings, open(output_folder + '/met_inp_{}_{}_{}_daywts.pkl'.format(catchment, output_dem, year_to_take), 'wb'), -1)
+    json.dump(config, open(output_folder + '/config_{}_{}_{}_{}_{}.json'.format(met_inp,catchment, output_dem, run_id, year_to_take), 'w'))
+    pickle.dump(config, open(output_folder + '/config_{}_{}_{}_{}_{}.pkl'.format(met_inp,catchment, output_dem, run_id, year_to_take), 'wb'), protocol=3)
+    pickle.dump(day_weightings, open(output_folder + '/met_inp_{}_{}_{}_{}_{}_daywts.pkl'.format(met_inp,catchment, output_dem, run_id, year_to_take), 'wb'), protocol=3)
