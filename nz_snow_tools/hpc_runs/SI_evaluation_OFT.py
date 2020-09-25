initial plots with fixed units

nz_snow_tools/hpc_runs/SI_evaluation_OFT.py
@@ -73,13 +73,14 @@ def SI_evaluation(run_id, met_inp, which_model, hydro_years_to_take, catchment, 
 
         # cut to same shape as MODIS data(2800L, 2540L), compared to SI dem of (2800L, 2560L), the W edge is further east in modis, so remove first 20 columns
         st_swe = st_swe[:, :, 20:]
+        st_swe[:, modis_mask == False] = np.nan
 
         # create binary snow cover
         st_sc = np.zeros(st_swe.shape, dtype=np.float32)  # needs to be float to handle nan's. Start with 0's i.e. no snow.
         st_sc[st_swe > model_swe_sc_threshold] = 1 # set to snow cover when over threshold
         st_sc[:, modis_mask == False] = np.nan  # set non-land points to np.nan. Includes inland water/ocean points from MODIS 2000-2016 and dem ==0
 
-        num_nan_model_gridpoints = np.sum(np.isnan(st_swe[:, modis_mask]), axis=0)
+        num_nan_model_gridpoints = np.sum(np.isnan(st_swe[:, modis_mask]), axis=0) #TODO change so that only counts if any time for each point
         print('{} land grid points with nan hydro year {}'.format(num_nan_model_gridpoints,hydro_year_to_take))
         ann_metadata['num_nan_model_gridpoints'][hydro_year_to_take] = num_nan_model_gridpoints
 