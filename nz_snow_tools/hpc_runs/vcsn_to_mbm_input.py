c531c643e62664b1b5ff8b2473b31928affef420
jonoconway <33852994+jonoconway@users.noreply.github.com>
2022-04-08 15:59:31 +1200

    updating comments

--- nz_snow_tools/hpc_runs/vcsn_to_mbm_input.py
+++ nz_snow_tools/hpc_runs/vcsn_to_mbm_input.py
@@ -1,5 +1,23 @@
 """
+ create hourly input for mass balance model from daily VCSN data
+
+ corrections to daily values
+air temperature: correction to AWSLake elevation (1650m) from VCSN elevation using lapse rate of 0.005 K/km. Then a separate seasonal linear bias correction for Tmin and Tmax
+wind and precip: daily data replaced with QMAP bias corrected values from Nariefa
+pressure: correction to AWSLake elevation using 1013.25 * (1 - (1 - elev_in / 44307.69231) ** 5.253283)
+
+
+Methods used to transform daily to hourly values:
+precip: random cascade
+air temperature: three part sinusoidal curve, with Tmin at 0600h and Tmax at 1500h based on mode of morning Tmin and afternoon Tmax for Brewster Glacier, respectively.
+rh: cubic interpolatation between 9am instantaneous values
+pressure: cubic interpolatation between 9am instantaneous values
+cloudiness: daily average values
+sw: hourly values as hourly TOA * daily observed fraction of TOA
+wind: daily average value
  use environment  pvlib38-x
+
+
 """
 
 import sys
@@ -24,40 +42,28 @@ def find_pressure_offset_elevation(elev_in, elev_out):
 sys.path.append('C:/Users/conwayjp/Documents/code/git_niwa_local/cloudglacier')
 from obj1.process_cloud_vcsn import process_daily_swin_to_cloud, ea_from_tc_rh
 
-# if len(sys.argv) == 2:
-#     config_file = sys.argv[1]
-#     config = yaml.load(open(config_file), Loader=yaml.FullLoader)
-#     print('reading configuration file')
-# else:
-#     print('incorrect number of commandline inputs')
-#
-
-# set up output times
-# first_time = parser.parse(config['output_file']['first_timestamp'])
-# last_time = parser.parse(config['output_file']['last_timestamp'])
 
 met_out_folder = 'C:/Users/conwayjp/OneDrive - NIWA/projects/MarsdenFS2018/Nariefa/vcsn'
-data_id = 'brewster_test_mbm_v4'
+data_id = 'brewster_full_mbm'
 target_elevation = 1650 # elevation to generate temperature and pressure output at
 
 # find lats and lons of closest points
 lat_to_take = [-44.075, -44.125]
 lon_to_take = [169.425, 169.475]
 
-first_time = dt.datetime(2009, 4, 1, 1, 0, tzinfo=dt.timezone(dt.timedelta(hours=12)))
+first_time = dt.datetime(1972, 4, 1, 1, 0, tzinfo=dt.timezone(dt.timedelta(hours=12)))
 last_time = dt.datetime(2021, 4, 1, 0, 0, tzinfo=dt.timezone(dt.timedelta(hours=12)))
 out_dt = pd.date_range(first_time, last_time, freq='1H').to_pydatetime()
 
-first_time_lt = dt.datetime(2009, 4, 1, 1, 0)
+first_time_lt = dt.datetime(1972, 4, 1, 1, 0)
 last_time_lt = dt.datetime(2021, 4, 1, 0, 0)
 out_dt_lt = pd.date_range(first_time_lt, last_time_lt, freq='1H').to_pydatetime()
 
 outfile = met_out_folder + '/met_inp_{}_{}_{}.dat'.format(data_id, first_time_lt.strftime('%Y%m%d%H%M'), last_time_lt.strftime('%Y%m%d%H%M'))
 
-# out_dt = np.asarray(make_regular_timeseries(first_time, last_time, 3600))
 print('time output from {} to {}'.format(first_time.isoformat(), last_time.isoformat()))
 
-# define location of input files
+# define location of original vcsn input files
 # vcsn_folder = '/scale_wlg_persistent/filesets/project/niwa00026/VCSN_grid'
 # vcsn_tmax_file = vcsn_folder + '/TMax_Norton/tmax_vclim_clidb_Norton_1972010200_2021090200_south-island_p05_daily_netcdf4.nc'# max air temp (C) over 24 hours FROM 9am on local day
 # vcsn_tmin_file = vcsn_folder + '/TMin_Norton/tmin_vclim_clidb_Norton_1972010200_2021090200_south-island_p05_daily_netcdf4.nc'# min air temp (C) over 24 hours TO 9 am on local day
@@ -66,7 +72,7 @@ print('time output from {} to {}'.format(first_time.isoformat(), last_time.isofo
 # vcsn_swin_file = vcsn_folder + '/SRad/srad_vclim_clidb_1972010200_2021090200_south-island_p05_daily_netcdf4.nc'# Total global solar radiation (MJ/m2) over 24 hours FROM midnight on local day
 # vcsn_ws_file = vcsn_folder + '/Wind/wind_vclim_clidb_1972010200_2021090200_south-island_p05_daily_netcdf4.nc'# Mean 10m wind speed (m/s) over 24 hours FROM midnight local day
 # vcsn_precip_file = vcsn_folder + '/Rain/rain_vclim_clidb_1972010200_2021090200_south-island_p05_daily_netcdf4.nc'# Total precip (mm) over 24 hours FROM 9am local day
-
+# example of how subset files were created:
 # ds_rain = xr.open_dataset('Rain/rain_vclim_clidb_1972010200_2021090200_south-island_p05_daily_netcdf4.nc')
 # ds_rain = ds_rain.assign_coords(longitude=ds_rain['longitude'].round(3)).assign_coords(latitude=ds_rain['latitude'].round(3))
 # ds_rain.sel(longitude=[169.425,169.475],latitude=[-44.075,-44.125]).to_netcdf('/nesi/project/niwa03098/rain.nc')
@@ -90,14 +96,9 @@ ds_mslp = xr.open_dataset(vcsn_mslp_file)
 ds_swin = xr.open_dataset(vcsn_swin_file)
 ds_ws = xr.open_dataset(vcsn_ws_file)
 ds_precip = xr.open_dataset(vcsn_precip_file)
-# ds_tmax_orig = xr.open_dataset(vcsn_tmax_original)
-# ds_tmin_orig = xr.open_dataset(vcsn_tmin_original)
-
 
 # update precip and wind variables from bias corrected files
-
 ds_precip_QMAP = xr.load_dataset(r"C:\Users\conwayjp\OneDrive - NIWA\projects\MarsdenFS2018\Nariefa\vcsn\to share\precip_QMAP.nc")
-
 precip_QMAP = np.zeros((ds_precip_QMAP['precip'][0, :].shape[0], 2, 2)) # set up array with extra dimensions then copy data in
 for i in range(2):
     for j in range(2):
@@ -108,7 +109,7 @@ ds_wind_QMAP = xr.load_dataset(r"C:\Users\conwayjp\OneDrive - NIWA\projects\Mars
 wind_QMAP = np.zeros((ds_ws['wind'].shape[0], 2, 2)) # set up array with extra dimensions then copy data in
 for i in range(2):
     for j in range(2):
-        wind_QMAP[10226:, i, j] = ds_wind_QMAP['wind'][0, :]
+        wind_QMAP[10226:, i, j] = ds_wind_QMAP['wind'][0, :] # 10226:
 ds_ws = ds_ws.assign(wind_bc=ds_ws['wind'].copy(data=wind_QMAP))# create bias corrected variable with same dimensions as original
 
 # load variables
@@ -134,24 +135,18 @@ inp_ws = inp_ws.assign_coords(time=('time', updated_time.data))
 
 inp_elev = ds_tmax['elevation'].sel(longitude=lon_to_take, latitude=lat_to_take)
 
-# inp_tmax_orig = ds_tmax_orig['tmax'].sel(time=slice(first_time, last_time), longitude=lon_to_take, latitude=lat_to_take) # TODO need to take value from next day as the variable is the max value over previous 24 hours.
-# inp_tmin_orig = ds_tmin_orig['tmin'].sel(time=slice(first_time, last_time), longitude=lon_to_take, latitude=lat_to_take)
-
 # spatial interpolation and bias correction
 hi_res_elev = inp_elev
-hi_res_precip = inp_precip  #
-
-
+# bias corrected air temperature
 def bias_correct(inp_var, slope, intercept):
     out_var = slope * inp_var + intercept
     return out_var
 
-
-tmax_month = np.asarray(inp_tmax[:, 0, 0].to_pandas().index.month)
-# [[1876., 1382.],[1277., 1420.]]
 offset_intercept = (inp_elev.values - target_elevation) * 0.005
+# [[1876., 1382.],[1277., 1420.]]
 
-hi_res_tmax = inp_tmax * np.nan  # use
+hi_res_tmax = inp_tmax * np.nan
+tmax_month = np.asarray(inp_tmax[:, 0, 0].to_pandas().index.month)
 for i, dat in enumerate(inp_tmax):
     # first correct for elevation offset and convert to C
     dat = bias_correct(dat, np.asarray([[1, 1], [1, 1]]), offset_intercept) - 273.16
@@ -171,8 +166,8 @@ for i, dat in enumerate(inp_tmax):
     # bias correct
     hi_res_tmax[i] = bias_correct(dat, bc_slope, bc_intercept)
 
+hi_res_tmin = inp_tmin * np.nan
 tmin_month = np.asarray(inp_tmin[:, 0, 0].to_pandas().index.month)
-hi_res_tmin = inp_tmin * np.nan  # use
 for i, dat in enumerate(inp_tmin):
     # first correct for elevation offset and convert to C
     dat = bias_correct(dat, np.asarray([[1, 1], [1, 1]]), offset_intercept) - 273.16
@@ -192,6 +187,7 @@ for i, dat in enumerate(inp_tmin):
     # bias correct
     hi_res_tmin[i] = bias_correct(dat, bc_slope, bc_intercept)
 
+hi_res_precip = inp_precip  #
 hi_res_rh = inp_rh  #
 p_offset = find_pressure_offset_elevation(0, target_elevation)
 hi_res_pres = inp_mslp + p_offset  #
@@ -203,8 +199,8 @@ hi_res_lats = ds_rh['latitude'].data
 hi_res_lons = ds_rh['longitude'].data
 
 # convert to hourly
-
-# send in data for start day to end date+1, then cut the first 15 hours and last 9 hours.
+# precip: multicative random cascade
+# precip: send in data for start day to end date+1, then cut the first 15 hours and last 9 hours.
 hourly_precip_full, day_weightings_full = process_precip(hi_res_precip.data)
 hourly_precip = hourly_precip_full[
                 15:-9]  # remove 15 hours data from day before and cut 9 hours from end (to align 24 hrs to 9am totals, to hourly totals midnight-midnight
@@ -217,9 +213,10 @@ hourly_precip = hourly_precip_full[
 hourly_temp = daily_to_hourly_temp_grids_new(hi_res_tmax.data, hi_res_tmin.data, time_min=6, time_max=15)
 hourly_temp = hourly_temp[24:-24]  # remove day either side
 
+#rh: cubic interpolatation between 9am instantaneous values
 hourly_rh = hi_res_rh.resample(time="1H").interpolate(kind='cubic')
 hourly_rh = hourly_rh[16:-9]  # trim to 1am on first day to midnight on last
-
+#presure: cubic interpolatation between 9am instantaneous values
 hourly_pres = hi_res_pres.resample(time="1H").interpolate(kind='cubic')
 hourly_pres = hourly_pres[16:-9]  # trim to 1am on first day to midnight on last
 
@@ -229,13 +226,14 @@ hourly_vp = ea_from_tc_rh(hourly_temp, hourly_rh)
 daily_vp = hourly_vp.data.reshape((-1, 24, hourly_temp.shape[1], hourly_temp.shape[2])).mean(axis=1)
 daily_tk = hourly_temp.reshape((-1, 24, hourly_temp.shape[1], hourly_temp.shape[2])).mean(axis=1) + 273.16
 daily_neff, daily_trc = process_daily_swin_to_cloud(hi_res_swin[:, 0, 0].to_pandas(), daily_vp[:, 0, 0], daily_tk[:, 0, 0], 1650, -45, 179)
-
+# cloudiness: daily avearge values
 hourly_neff = (np.expand_dims(daily_neff, axis=1) * np.ones(24)).reshape(daily_neff.shape[0] * 24)
 hourly_neff = xr.DataArray(hourly_neff, hourly_rh[:, 0, 0].coords, name='neff')
 hourly_trc = (np.expand_dims(daily_trc, axis=1) * np.ones(24)).reshape(daily_trc.shape[0] * 24)
 hourly_trc = xr.DataArray(hourly_trc, hourly_rh[:, 0, 0].coords, name='trc')
+# sw: hourly values as hourly TOA * daily observed fraction of TOA
 hourly_swin = daily_to_hourly_swin_grids_new(hi_res_swin.data, hi_res_lats, hi_res_lons, out_dt_lt)
-
+# wind: daily average value
 hourly_ws = hi_res_ws.resample(time="1H").bfill()[1:]  # fill in average value and remove first timestamp
 
 # merge into one dataset
